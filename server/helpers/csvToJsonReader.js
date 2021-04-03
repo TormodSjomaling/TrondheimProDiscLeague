7e53c7292b4b992bd0275f8f939b248cd9224d26
Tormod <tormodls@stud.ntnu.no>
2021-04-03 16:32:41 +0200

    namechange

--- server/helpers/csvToJsonReader.js
+++ server/helpers/csvToJsonReader.js
@@ -5,6 +5,18 @@ let testDataRound1 = "PlayerName,CourseName,LayoutName,Dato,Totalt,+/-,Hole1,Hol
 "Martin,Dragvoll Diskgolfarena,Dragvoll 2021,2021-03-27 10:36,68,12,3,6,2,4,5,4,3,4,3,4,3,3,4,4,3,4,6,3 \n" +
 "Morten,Dragvoll Diskgolfarena,Dragvoll 2021,2021-03-27 10:36,70,14,3,5,3,3,3,5,5,4,4,3,7,3,3,4,3,4,4,4";
 
+let testDataRound2 = "PlayerName,CourseName,LayoutName,Dato,Totalt,+/-,Hole1,Hole2,Hole3,Hole4,Hole5,Hole6,Hole7,Hole8,Hole9,Hole10,Hole11 \n" +
+"Par,Rotvoll Diskgolfpark,Good tees,2021-04-03 13:23,34,,3,3,3,3,3,3,4,3,3,3,3 \n" +
+"Andreas,Rotvoll Diskgolfpark,Good tees,2021-04-03 13:23,42,8,3,4,4,4,5,3,5,3,3,4,4 \n" +
+"Tormod,Rotvoll Diskgolfpark,Good tees,2021-04-03 13:23,39,5,3,4,3,4,5,4,4,3,3,3,3 \n" +
+"Martin,Rotvoll Diskgolfpark,Good tees,2021-04-03 13:23,39,5,3,4,3,3,5,3,5,2,5,3,3"
+
+let testDataRound3 = "PlayerName,CourseName,LayoutName,Dato,Totalt,+/-,Hole1,Hole2,Hole3,Hole4,Hole5,Hole6,Hole7,Hole8,Hole9,Hole10,Hole11 \n" +
+"Par,Rotvoll Diskgolfpark,Good tees,2021-04-03 14:31,34,,3,3,3,3,3,3,4,3,3,3,3 \n" +
+"Andreas,Rotvoll Diskgolfpark,Good tees,2021-04-03 14:31,46,12,4,4,4,4,5,6,5,4,4,4,2 \n" +
+"Martin,Rotvoll Diskgolfpark,Good tees,2021-04-03 14:31,42,8,3,4,4,4,5,3,7,3,3,3,3 \n" +
+"Tormod,Rotvoll Diskgolfpark,Good tees,2021-04-03 14:31,45,11,3,3,4,4,4,6,8,3,3,4,3"
+
 const csvJsonReader = (csv) =>{
 
     var lines=csv.split("\n");
@@ -34,7 +46,7 @@ const csvJsonReader = (csv) =>{
     return JSON.stringify(roundDocumentJson);
   }
 
-//let result = csvJsonReader(testDataRound1);
-//console.log(result)
+let result = csvJsonReader(testDataRound3);
+console.log(result)
 
 export default csvJsonReader;
